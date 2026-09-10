dbe503ecb6b146797e156261db52c158c6ba03fd
wiktorszczepanik <126973859+wiktorszczepanik@users.noreply.github.com>
2026-09-10 22:43:00 +0200

    Parser and Image to Pattern conversion

---
+++ input/parser.go
@@ -0,0 +1,96 @@
+package input
+
+import (
+	"compress/gzip"
+	"encoding/binary"
+	"errors"
+	"io"
+	"os"
+)
+
+type Image struct {
+	Label  byte
+	Pixels []byte
+}
+
+func GetImages(labelPath, pixelsPath string) (images []Image, err error) {
+	labels, err := loadLabels(labelPath)
+	if err != nil {
+		return nil, err
+	}
+	pixels, step, err := loadPixels(pixelsPath)
+	if err != nil {
+		return nil, err
+	}
+	images = make([]Image, len(labels))
+	for i, label := range labels {
+		start := i * step
+		end := start + step
+		images[i] = Image{label, pixels[start:end]}
+	}
+	return images, nil
+}
+
+func loadPixels(path string) (pixels []byte, step int, err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer file.Close()
+	gz, err := gzip.NewReader(file)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer gz.Close()
+	var magic, count, rows, cols uint32
+	if err := binary.Read(gz, binary.BigEndian, &magic); err != nil {
+		return nil, 0, err
+	}
+	if err := binary.Read(gz, binary.BigEndian, &count); err != nil {
+		return nil, 0, err
+	}
+	if err := binary.Read(gz, binary.BigEndian, &rows); err != nil {
+		return nil, 0, err
+	}
+	if err := binary.Read(gz, binary.BigEndian, &cols); err != nil {
+		return nil, 0, err
+	}
+	if magic != 2051 {
+		return nil, 0, errors.New("invalid image magic number")
+	}
+	step = int(rows * cols)
+	pixels = make([]byte, step*int(count))
+	if _, err := io.ReadFull(gz, pixels); err != nil {
+		return nil, 0, err
+	}
+	return pixels, step, nil
+}
+
+func loadLabels(path string) (labels []byte, err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	gz, err := gzip.NewReader(file)
+	if err != nil {
+		return nil, err
+	}
+	defer gz.Close()
+	var magic, count uint32
+	if err := binary.Read(gz, binary.BigEndian, &magic); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(gz, binary.BigEndian, &count); err != nil {
+		return nil, err
+	}
+	if magic != 2049 {
+		return nil, errors.New("invalid image magic number")
+	}
+	labels = make([]byte, int(count))
+	if _, err := io.ReadFull(gz, labels); err != nil {
+		return nil, err
+	}
+	return labels, nil
+
+}
--- network/pattern.go
+++ network/pattern.go
@@ -1,12 +1,28 @@
 package network
 
+import "artificial-neural-network/input"
+
+// Pattern MNIST dataset
 type Pattern struct {
-	Features            []float64
-	MultipleExpectation []float64
+	Features            [784]float64
+	MultipleExpectation [10]float64
 }
 
-func (pattern Pattern) scaleFeatures() {
-	//for i := range pattern.Features {
-	//	pattern.Features[i] = Sigmoid(pattern.Features[i])
-	//}
+func ConvertFromImages(images []input.Image) (patterns []Pattern) {
+	patterns = make([]Pattern, len(images))
+	for i := range images {
+		// Images Pixels to Pattern Features
+		for j := range images[i].Pixels {
+			patterns[i].Features[j] = float64(images[i].Pixels[j] / 255.0)
+		}
+		// Images Label to Pattern Expectations
+		for j := range 10 {
+			if int(images[i].Label) == j {
+				patterns[i].MultipleExpectation[j] = 1
+			} else {
+				patterns[i].MultipleExpectation[j] = 0
+			}
+		}
+	}
+	return patterns
 }
